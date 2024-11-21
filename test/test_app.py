6ce4950609da15a9669778dfff2304e6bc29150a
leaazar <lea.g.azar1@gmail.com>
2024-11-21 16:10:36 +0200

    Your commit message here

--- test/test_app.py
+++ test/test_app.py
@@ -1,10 +1,9 @@
+# tests/test_app.py
 import unittest
 from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
-
-
 if __name__ == "__main__":
     unittest.main()
